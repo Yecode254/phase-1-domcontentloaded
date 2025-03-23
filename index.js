b490a8f908bd101eaafed750df19947444e27543
Yego <eddieyego273@gmail.com>
2025-03-23 21:47:27 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    const p = document.querySelector('P')
+    p.textContent = "This is really cool!"
+  });
